ca5e5b5a614a4fd4d86b909e95b77b3e7ebb2191
Chi Zhang <chizhg@google.com>
2026-02-26 01:08:00 -0800

    Update CatController

--- src/main/java/com/example/demo/CatController.java
+++ src/main/java/com/example/demo/CatController.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @Slf4j
